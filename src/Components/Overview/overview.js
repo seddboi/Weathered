e65af1cecae8a54e653cccb529ac21b78648cc61
Gian Zamora <gz2016@att.net>
2021-10-09 22:52:33 -0700

    first commit

---
+++ src/Components/Overview/overview.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './overview.css';
+
+function Overview() {
+    return (
+        <div>
+            <div class='jumbotron container' id='heading-title'>
+                <h1>Welcome to <strong>Weathered</strong>!</h1>
+                <h3>A Simple React-Based Weather Dashboard </h3>
+            </div>
+
+            <div class='container' id='homepage-skeleton'>
+                <div class='d-flex'>
+                    <section class='' id='search-bar-recents'>
+                        <h3 class='m-3'>Recents</h3>
+                        <div id='insert-recent-buttons'>
+
+                        </div>
+
+                        <div class="m-3">
+                            <label for="exampleFormControlInput1" class="form-label">Search A New City</label>
+                            <input class="form-control" id="search-bar" placeholder="Las Vegas, etc." />
+                        </div>
+                    </section>
+
+                    <section class='flex-grow-1' id='forecast'>
+                        <h3 class='m-3'>Hourly Forecast for <span id='hourly-date'></span></h3>
+                    </section>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Overview;
