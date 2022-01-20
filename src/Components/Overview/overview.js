d519c719ed4411d6a3a318cf44f0c87ceb87f9a3
Gian Zamora <gz2016@att.net>
2022-01-19 16:30:45 -0800

    added npm packages necessary for simple backend for dotenv purposes

--- src/Components/Overview/overview.js
+++ src/Components/Overview/overview.js
@@ -1,77 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './overview.css';
 import { RecentSearches } from '../Recent Searches/recentSearches';
 import { Hourly } from '../Hourly/hourly';
 import { Fiveday } from '../5 Day Forecast/fiveday';
-
-require('dotenv').config(); 
-
-const fetchForecast = async (city) => {
-
-    const api_key = process.env.APIKEY;
-    // const api_key = '2220f5a5d83243938f764759211310';
-    var BASE_URL = `http://api.weatherapi.com/v1/forecast.json?key=${api_key}&q=${city}&days=3&aqi=no&alerts=no`;
-
-    const response = await fetch(BASE_URL);
-    const data = await response.json();
-    console.log(data);
-    // collects all of the current weather info for your search
-    const currentTempInfo = {
-        city: data.location.name, 
-        state: data.location.region, 
-        epochDate: data.location.localtime_epoch, 
-        message: data.current.condition.text, 
-        wicon: data.current.condition.icon, 
-        currentTemp: data.current.temp_f,
-        currentHighTemp: data.forecast.forecastday[0].day.maxtemp_f,
-        currentLowTemp: data.forecast.forecastday[0].day.mintemp_f,
-        feelsLike: data.current.feelslike_f,
-        humidity: data.current.humidity,
-        rainLevel: data.current.precip_in,
-        // hourlyTemps is an array, starts from midnight(index 0) and goes every hour until 11 pm(index 23)
-        hourlyTemps: data.forecast.forecastday[0].hour.map((entry) => {
-            let epochTime, temp;
-            [epochTime, temp] = [entry.time_epoch, entry.temp_f];
-            return [epochTime, temp];
-        })
-    };
-
-    const daycardInfo = [];
-
-    // this for loop triggers and creates an array with all necessary values for the 
-    for (var x in data) {
-        const fcDayDates = data.forecast.forecastday[x].date_epoch;
-        const dayInfo = data.forecast.forecastday[x].day; 
-        const dayValues = {
-            dates: fcDayDates, 
-            message: dayInfo.condition.text, 
-            wicon: dayInfo.condition.icon, 
-            maxTemp: dayInfo.maxtemp_f, 
-            minTemp: dayInfo.mintemp_f, 
-            avgTemp: dayInfo.avgtemp_f
-        };
-        // pushes dayValues object into daycardInfor array
-        daycardInfo.push(dayValues);    
-    };
-
-    // this updates the state with the forecast for the city entered
-    const newData = {
-        currentTempInfo: currentTempInfo,
-        daycardInfo: daycardInfo
-    };
-
-    // this spits out the newly created forecast object
-    return newData;
-};
+import { fetchForecast } from '../../helpers/fetchForecast';
 
 export function Overview() {
-    const [search, setSearch] = useState(null);
 
-    // 
+    // this callback function receives the searched city entered from recentSearches and applies it to fetchForecast
     const getSearch = async (searchedCity) => {
-        setSearch(searchedCity);
-        return await fetchForecast(search);
-    };
+        fetchForecast(searchedCity);
+    }; 
 
     return (
         <div>
