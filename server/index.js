d4ea38ad3ddbe47cfa891fae4d471384cd6af7c0
Gian Zamora <gz2016@att.net>
2022-02-17 09:45:30 -0800

    adjusted package.json for static server for client build

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 
-app.use(express.static(path.resolve(__dirname, 'build')))
+app.use(express.static(path.resolve(__dirname, './client/build')))
 
 app.get('/', (req, res) => {
     res.json('hi');
