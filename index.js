2f5b0063a7c540ef04d66d52aa3993094b8cc0c8
Gian Zamora <gz2016@att.net>
2022-02-17 10:49:30 -0800

    fixed up public folder to host js code for react

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 
-app.use(express.static(path.resolve(__dirname, '../client/build')))
+app.use(express.static("public"))
 
 app.get('/', (req, res) => {
     res.json('hi');
